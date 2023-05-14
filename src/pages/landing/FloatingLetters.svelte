f7215e92c00180d2c1104a7ffd56372b4d1b3f7f
EggbertFluffle <81518632+EggbertFluffle@users.noreply.github.com>
2023-05-14 11:37:08 +0000

    Finished floating letters with p5.js

--- src/pages/landing/FloatingLetters.svelte
+++ src/pages/landing/FloatingLetters.svelte
@@ -1,38 +1,95 @@
 <script>
 	import P5 from "p5-svelte";
 
-	const width = window.innerWidth;
-	const height = window.innerHeight;
-
-	let testCommit = true;
+	export let width;
+	export let height;
+	export let text;
 	
-	const sketch = (p5) => {		
+	const sketch = (p5) => {
+		p5.Vector.zero = p5.createVector(0, 0);
+		
 		class Letter{
 		  constructor(_letter, tx, ty){
 		    this.letter = _letter;
 		    this.target = p5.createVector(tx, ty);
-		    this.rotation = p5.random(-180, 180);
-		    this.position = p5.createVector(
+		    this.rot = p5.random(-180, 180);
+		    this.pos = p5.createVector(
 		      this.target.x + p5.random(-250, 250),
 		      this.target.y + p5.random(0, 250)
 		    );
-				this.move = () => {
-			    this.position.lerp(this.target, 0.07);
-			    this.rotation *= 0.92;
-			  };
-				this.display = () => {
-					p5.translate(this.position.x, this.position.y);
-			    p5.rotate(this.rotation);
-			    p5.text(this.letter, 0, 0);
+				this.vel = p5.Vector.random2D();
+				this.acc = p5.createVector();
+				this.maxSpeed = 15;
+				this.maxForce = 1;
+				
+				this.behaviors = () => {
+					let arrive = this.arrive(this.target);
+					let mouse = p5.createVector(p5.mouseX, p5.mouseY);
+					let flee = this.flee(mouse);
+
+					arrive.mult(3);
+					flee.mult(4);
+					
+					this.applyForce(arrive);
+					this.applyForce(flee);
+				};
+				
+				this.applyForce = (force) => {
+					this.acc.add(force);
 				}
+				
+				this.update = () => {
+			    this.pos.add(this.vel);
+					this.vel.add(this.acc);
+					this.acc.mult(0, 0);
+			  };
+				
+				this.display = () => {
+					p5.translate(this.pos.x, this.pos.y);
+			    p5.rotate(this.rot);
+			    p5.text(this.letter, 0, 0);
+				};
+				
+				this.flee = (target) => {
+			    var desired = p5.Vector.sub(target, this.pos);
+			    var d = desired.mag();
+			    if (d < fontSize) {
+			      desired.setMag(this.maxSpeed);
+			      desired.mult(-1);
+			      var steer = p5.Vector.sub(desired, this.vel);
+			      steer.limit(this.maxForce);
+			      return steer;
+			    } else {
+			      return p5.Vector.zero;
+			    }
+			  }
+
+				this.arrive = (target) => {
+			    var desired = p5.Vector.sub(target, this.pos);
+			    var d = desired.mag();
+			    var speed = this.maxSpeed;
+			    if (d < fontSize) {
+			      speed = p5.map(d, 0, 100, 0, this.maxSpeed);
+			    }
+			    desired.setMag(speed);
+			    var steer = p5.Vector.sub(desired, this.vel);
+			    steer.limit(this.maxForce);
+			    return steer;
+			  }
+				
+				this.isFinished = () => {
+					return Math.abs(this.target.x - this.pos.x) < 0.1 && 
+						Math.abs(this.target.y - this.pos.y) < 0.1 && 
+						Math.abs(this.rot) < 0.01;
+				};
 		  }
 		}
 
-		const fontSize = 98;
-		const kerning = 125;
-		const word = "EGGBERT";
+		const word = text || "Hello!";
 		
 		let letters = word.split("");
+
+		const fontSize = 256;
 		
 		p5.setup = () => {
 			p5.createCanvas(width, height);
@@ -40,26 +97,32 @@
 			p5.textAlign(p5.CENTER, p5.CENTER);
 			p5.angleMode(p5.DEGREES);
 			p5.textStyle(p5.BOLD);
-			p5.textSize(98);
+			p5.textSize(fontSize);
 			p5.fill(255);
 
 			for(let i = 0; i < letters.length; i++) {
-		    letters[i] = new Letter(letters[i], (i * kerning) + 100, height / 3);
+		    letters[i] = new Letter(letters[i], (i * fontSize) + 100, height / 3);
 		  }
 		}
 
 		p5.draw = () => {
 			p5.background(0, 0, 0, 115);
 
-			letters.forEach((l) => {
+			letters.forEach(l => {
 				p5.resetMatrix();
-				l.move();
+				l.behaviors();
+				l.update();
 				l.display();
 			});
+
+			if(letters.every(l => {
+				return l.isFinished();
+			})) {
+				console.log("Floating letters animation concluded.");
+				p5.noLoop();
+			}
 		}
 	}
-
-	
 </script>
 
 <main>
@@ -67,5 +130,8 @@
 </main>
 
 <style>
-	
+	main {
+		padding: 0px;
+		margin: 0px;
+	}
 </style>
--- src/pages/landing/Landing.svelte
+++ src/pages/landing/Landing.svelte
@@ -1,11 +1,21 @@
 <script>
 	import FloatingLetters from "./FloatingLetters.svelte";
+	import { onMount } from 'svelte';
+
+	let container;
 </script>
 
-<main>
-	<FloatingLetters />
+<main bind:this={container}>
+	<FloatingLetters text="Hola" width={window.innerWidth} height={window.innerHeight} />
 </main>
 
 <style>
-	
+	main {
+		width: 100%;
+		height: 100%;
+
+		background-color: red;
+		padding: 0px;
+		margin: 0px;
+	}
 </style>
